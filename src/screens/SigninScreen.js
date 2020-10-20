4d8903223929a65b4c606f45f796bb214f64a6fa
smulionOong <1224police@likelion.org>
2020-10-20 15:46:16 +0900

    first commit

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SigninScreen = () => {
+  return (
+    <View>
+      <Text style={{fontSize:48}}>signinScreen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
